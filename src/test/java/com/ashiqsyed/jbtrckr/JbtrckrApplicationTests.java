616660b4e9bee685c201a5838132d747b08644b5
ashiqsyed <ahs57816@uga.edu>
2025-06-25 14:16:34 -0400

    got test request to work, need to work on crud action implementations

---
+++ src/test/java/com/ashiqsyed/jbtrckr/JbtrckrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashiqsyed.jbtrckr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbtrckrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
